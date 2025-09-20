1cb97966201b9bddefd6e40d6f9bf5c93481f861
Sadid Rahman <ich.heisse.sadid@gmail.com>
2025-09-20 13:33:10 +0930

    navbar complete

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,20 +1,27 @@
 function toggleMenu() {
     var navbar = document.querySelector(".navbar");
-    var left = document.querySelector(".nav_left");
-    var center = document.querySelector(".nav_center");
-    var right = document.querySelector(".nav_right");
     var menu_btn = document.querySelector(".menu-icon");
+    const search_form = document.getElementById("search-form-mobile");
 
     if (navbar.classList.contains("open")) {
         // Collapse
         navbar.classList.remove("open");
         menu_btn.innerHTML = "&#9776;";
         menu_btn.style.fontSize = "20px";
+        menu_btn.style.textAlign = "left";
+        menu_btn.style.width = "auto";
+        search_form.style.display = "none";
+
     } else {
         // Expand
         navbar.classList.add("open");
         menu_btn.innerHTML = "&uarr;";
         menu_btn.style.fontSize = "25px";
+        menu_btn.style.cursor = "pointer";
+        menu_btn.style.width = "100%";
+        menu_btn.style.textAlign = "center";
+        search_form.style.display = "none";
+
     }
 }
 
@@ -23,10 +30,15 @@ window.addEventListener("resize", () => {
     if (window.innerWidth > 715) {
         var navbar = document.querySelector(".navbar");
         var menu_btn = document.querySelector(".menu-icon");
+        const search_form = document.getElementById("search-form-mobile");
 
         // Remove mobile menu styles
         navbar.classList.remove("open");
         menu_btn.innerHTML = "&#9776;";
         menu_btn.style.fontSize = "20px";
+        menu_btn.style.textAlign = "left";
+        menu_btn.style.width = "auto";
+        search_form.style.display = "none";
     }
 });
+
